ed4c30882c55ca5e239ff1c9d32eb5d2dde137d6
Grig <grigor77.ait@gmail.com>
2024-01-16 18:43:43 +0400

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Route, Routes } from 'react-router-dom'
+import Main_Layout from './layouts/MainLayout'
+import Home from './pages/Home'
 function App() {
 
 
   return (
     <>
-    
+    <Routes>
+      <Route path='/' element={<Main_Layout />}>
+        <Route  path='/' element={<Home />}/>
+      </Route>
+    </Routes>
     </>
   )
 }
